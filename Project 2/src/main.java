bc6bc75bc996d9a67601fda91e1cba8daca50cbc
David Chen <100171715+Kazvto123@users.noreply.github.com>
2022-09-27 18:51:27 -0400

    Update main.java

--- Project 2/src/main.java
+++ Project 2/src/main.java
@@ -1,96 +1,99 @@
 import java.sql.*;
-import java.util.Scanner;
      
 public class main  {
 
   public static void main(String args[]) {
-/* Queries 
-1. Select dept_name,
-avg(if(employees.gender = 'F', salaries.salary,0)) /avg(if(employees.gender = 'M', salaries.salary,0)) as 'Ratio'
-from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no
-inner join salaries on salaries.emp_no = employees.emp_no
-join departments on  departments.dept_no = dept_emp.dept_no
-group by dept_name
-order by ratio desc;
-
-2. Select employees.first_name,employees.last_name, datediff(dept_manager.to_date,dept_manager.from_date) as 'Duration'
-From dept_manager join employees
-where dept_manager.emp_no = employees.emp_no
-Order by Duration Desc;
-
-3. SELECT DISTINCT departments.dept_name, count(employees.emp_no), AVG(salaries.salary),
-ROUND(YEAR(employees.birth_date), -1) AS birth_date 
-FROM employees
-JOIN salaries ON employees.emp_no = salaries.emp_no
-JOIN dept_emp ON dept_emp.emp_no = employees.emp_no 
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-GROUP BY departments.dept_name, ROUND(YEAR(employees.birth_date), -1);
-
-4. Select employees.first_name, employees.last_name, birth_date, salary, gender
-From employees join salaries on employees.emp_no = salaries.emp_no
-join dept_manager on dept_manager.emp_no = employees.emp_no
-and salary>80000 and gender = 'F' and birth_date < cast('1990-01-01' as date)
-group by first_name;
-
-
-
-
-	   
-*/
-	  
-      
-    String url = "jdbc:mysql://localhost:3306/employees";
-    Connection con;
-	System.out.println("Enter SQL Query: "); // Do not press enter for new line. Pressing enter will submit the query.
-    Scanner scan = new Scanner(System.in);
+	System.out.println("Query 1");
+    runSQL("Select dept_name,\r\n"
+    		+ "	  avg(if(employees.gender = 'F', salaries.salary,0)) /avg(if(employees.gender = 'M', salaries.salary,0)) as 'Ratio'\r\n"
+    		+ "	  from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no\r\n"
+    		+ "	  inner join salaries on salaries.emp_no = employees.emp_no\r\n"
+    		+ "	  join departments on  departments.dept_no = dept_emp.dept_no\r\n"
+    		+ "	  group by dept_name\r\n"
+    		+ "	  order by ratio desc; ");
     
-    String query = " "; // Copy and paste queries here!
-    Statement stmt;
+    System.out.println("Query 2");
+    runSQL("Select employees.first_name,employees.last_name, datediff(dept_manager.to_date,dept_manager.from_date) as 'Duration'\r\n"
+    		+ "From dept_manager join employees\r\n"
+    		+ "where dept_manager.emp_no = employees.emp_no\r\n"
+    		+ "Order by Duration Desc;\r\n"
+    		+ "");
+    
+    System.out.println("Query 3");
+    runSQL("SELECT DISTINCT departments.dept_name, count(employees.emp_no), AVG(salaries.salary),\r\n"
+    		+ "ROUND(YEAR(employees.birth_date), -1) AS birth_date \r\n"
+    		+ "FROM employees\r\n"
+    		+ "JOIN salaries ON employees.emp_no = salaries.emp_no\r\n"
+    		+ "JOIN dept_emp ON dept_emp.emp_no = employees.emp_no \r\n"
+    		+ "JOIN departments ON dept_emp.dept_no = departments.dept_no\r\n"
+    		+ "GROUP BY departments.dept_name, ROUND(YEAR(employees.birth_date), -1);\r\n"
+    		+ "");
+    
+    System.out.println("Query 4");
+    runSQL("Select employees.first_name, employees.last_name, birth_date, salary, gender\r\n"
+    		+ "From employees join salaries on employees.emp_no = salaries.emp_no\r\n"
+    		+ "join dept_manager on dept_manager.emp_no = employees.emp_no\r\n"
+    		+ "and salary>80000 and gender = 'F' and birth_date < cast('1990-01-01' as date)\r\n"
+    		+ "group by first_name;\r\n"
+    		+ "");
+    
+    
+    
   
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
   
-    } catch(java.lang.ClassNotFoundException e) {
-      System.err.print("ClassNotFoundException: "); 
-      System.err.println(e.getMessage());
-    }
-
-    try {
-      con = DriverManager.getConnection(url, 
-                   "root", "Dyc30600.1"); // Enter your server credentials
-  
-      stmt = con.createStatement();              
-  
-      ResultSet rs = stmt.executeQuery(query);
-      ResultSetMetaData rsmd = rs.getMetaData();
-  
-      System.out.println("");
-  
-      int numberOfColumns = rsmd.getColumnCount();
-  
-      for (int i = 1; i <= numberOfColumns; i++) {
-        if (i > 1) System.out.print(",  ");
-        String columnName = rsmd.getColumnName(i);
-        System.out.print(columnName);
-      }
-      System.out.println("");
-  
-      while (rs.next()) {
-        for (int i = 1; i <= numberOfColumns; i++) {
-          if (i > 1) System.out.print(",  ");
-          String columnValue = rs.getString(i);
-          System.out.print(columnValue);
-        }
-        System.out.println("");  
-      }
-  
-      stmt.close();
-      con.close();
-    } catch(SQLException ex) {
-      System.err.print("SQLException: ");
-      System.err.println(ex.getMessage());
-    }  
-  }
 }
+
+
+public static void runSQL(String query) {
+	String url = "jdbc:mysql://localhost:3306/employees";
+    Connection con;
+    Statement stmt;
+	try {
+	      Class.forName("com.mysql.cj.jdbc.Driver");
+	  
+	    } catch(java.lang.ClassNotFoundException e) {
+	      System.err.print("ClassNotFoundException: "); 
+	      System.err.println(e.getMessage());
+	    }
+
+	    try {
+	      con = DriverManager.getConnection(url, 
+	                   "root", "Dyc30600.1"); // Enter your server credentials
+	  
+	      stmt = con.createStatement();              
+	  
+	      ResultSet rs = stmt.executeQuery(query);
+	      ResultSetMetaData rsmd = rs.getMetaData();
+	  
+	  
+	      int numberOfColumns = rsmd.getColumnCount();
+	  
+	      for (int i = 1; i <= numberOfColumns; i++) {
+	        if (i > 1) System.out.print(",  ");
+	        String columnName = rsmd.getColumnName(i);
+	        System.out.print(columnName);
+	      }
+	      System.out.println("");
+	  
+	      while (rs.next()) {
+	        for (int i = 1; i <= numberOfColumns; i++) {
+	          if (i > 1) System.out.print(",  ");
+	          String columnValue = rs.getString(i);
+	          System.out.print(columnValue);
+	        }
+	        System.out.println("");  
+	      }
+	      System.out.println();
+	      stmt.close();
+	      con.close();
+	    } catch(SQLException ex) {
+	      System.err.print("SQLException: ");
+	      System.err.println(ex.getMessage());
+	    }  
+	  }
+}
+
 	
+
+
 
