50505600dde987bd2056a79ea44a3598593033dc
bhavyasanghi <63983271+bhavyasanghi@users.noreply.github.com>
2022-09-27 22:43:24 -0400

    Added Query 5

--- Project 2/src/main.java
+++ Project 2/src/main.java
@@ -1,41 +1,72 @@
 import java.sql.*;
-     
+import java.util.*;     
 public class main  {
 
   public static void main(String args[]) {
-	System.out.println("Query 1");
-    runSQL("Select dept_name,\r\n"
+	Scanner sc = new Scanner(System.in);
+	int input = 0;
+	System.out.println("Which Query do you want to test?");
+	input = sc.nextInt();
+	switch(input)
+	{
+		case 1:
+			System.out.println("Query 1");
+    		runSQL("Select dept_name,\r\n"
     		+ "	  avg(if(employees.gender = 'F', salaries.salary,0)) /avg(if(employees.gender = 'M', salaries.salary,0)) as 'Ratio'\r\n"
     		+ "	  from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no\r\n"
     		+ "	  inner join salaries on salaries.emp_no = employees.emp_no\r\n"
     		+ "	  join departments on  departments.dept_no = dept_emp.dept_no\r\n"
     		+ "	  group by dept_name\r\n"
     		+ "	  order by ratio desc; ");
+			break;
+		case 2:
+			System.out.println("Query 2");
+			runSQL("Select employees.first_name,employees.last_name, datediff(dept_manager.to_date,dept_manager.from_date) as 'Duration'\r\n"
+				+ "From dept_manager join employees\r\n"
+				+ "where dept_manager.emp_no = employees.emp_no\r\n"
+				+ "Order by Duration Desc;\r\n"
+				+ "");
+			break;
+		case 3:
+			System.out.println("Query 3");
+			runSQL("SELECT DISTINCT departments.dept_name, count(employees.emp_no), AVG(salaries.salary),\r\n"
+				+ "ROUND(YEAR(employees.birth_date), -1) AS birth_date \r\n"
+				+ "FROM employees\r\n"
+				+ "JOIN salaries ON employees.emp_no = salaries.emp_no\r\n"
+				+ "JOIN dept_emp ON dept_emp.emp_no = employees.emp_no \r\n"
+				+ "JOIN departments ON dept_emp.dept_no = departments.dept_no\r\n"
+				+ "GROUP BY departments.dept_name, ROUND(YEAR(employees.birth_date), -1);\r\n"
+				+ "");
+				break;
+		case 4:
+			System.out.println("Query 4");
+			runSQL("Select employees.first_name, employees.last_name, birth_date, salary, gender\r\n"
+				+ "From employees join salaries on employees.emp_no = salaries.emp_no\r\n"
+				+ "join dept_manager on dept_manager.emp_no = employees.emp_no\r\n"
+				+ "and salary>80000 and gender = 'F' and birth_date < cast('1990-01-01' as date)\r\n"
+				+ "group by first_name;\r\n"
+				+ "");
+				break;
+		case 5:
+			System.out.println("Query 5");
+			String e1;
+			String e2;
+			System.out.println("Give Employee 1 id: ");
+			e1 = sc.next();
+			System.out.println("Give Employee 2 id: ");
+			e2 = sc.next();
+			String query = "Select dept_no From employees inner join dept_emp on employees.emp_no = dept_emp.emp_no where employees.emp_no = E1 and dept_emp.dept_no in (Select dept_no from dept_emp inner join employees on employees.emp_no = dept_emp.emp_no where employees.emp_no = E2) limit 100;";
+
+			query = query.replace("E1",e1);
+			query = query.replace("E2",e2);
+			runSQL(query);
+			break;
+	}
+
     
-    System.out.println("Query 2");
-    runSQL("Select employees.first_name,employees.last_name, datediff(dept_manager.to_date,dept_manager.from_date) as 'Duration'\r\n"
-    		+ "From dept_manager join employees\r\n"
-    		+ "where dept_manager.emp_no = employees.emp_no\r\n"
-    		+ "Order by Duration Desc;\r\n"
-    		+ "");
     
-    System.out.println("Query 3");
-    runSQL("SELECT DISTINCT departments.dept_name, count(employees.emp_no), AVG(salaries.salary),\r\n"
-    		+ "ROUND(YEAR(employees.birth_date), -1) AS birth_date \r\n"
-    		+ "FROM employees\r\n"
-    		+ "JOIN salaries ON employees.emp_no = salaries.emp_no\r\n"
-    		+ "JOIN dept_emp ON dept_emp.emp_no = employees.emp_no \r\n"
-    		+ "JOIN departments ON dept_emp.dept_no = departments.dept_no\r\n"
-    		+ "GROUP BY departments.dept_name, ROUND(YEAR(employees.birth_date), -1);\r\n"
-    		+ "");
     
-    System.out.println("Query 4");
-    runSQL("Select employees.first_name, employees.last_name, birth_date, salary, gender\r\n"
-    		+ "From employees join salaries on employees.emp_no = salaries.emp_no\r\n"
-    		+ "join dept_manager on dept_manager.emp_no = employees.emp_no\r\n"
-    		+ "and salary>80000 and gender = 'F' and birth_date < cast('1990-01-01' as date)\r\n"
-    		+ "group by first_name;\r\n"
-    		+ "");
+    
     
     
     
@@ -58,7 +89,7 @@ public static void runSQL(String query) {
 
 	    try {
 	      con = DriverManager.getConnection(url, 
-	                   "root", "Dyc30600.1"); // Enter your server credentials
+	                   "root", "Sanghi@009*"); // Enter your server credentials
 	  
 	      stmt = con.createStatement();              
 	  
